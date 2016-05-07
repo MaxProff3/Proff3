364523f86370c4e8a9e68eb3e08384ded6ede371
Kayenushka <kayen@ukr.net>
2016-05-08 00:07:45 +0300

    calc update

--- igor_tkanka/src/main/java/calc/View.java
+++ igor_tkanka/src/main/java/calc/View.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class View implements Initializable {
