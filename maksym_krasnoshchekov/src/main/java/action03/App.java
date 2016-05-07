c4955f40fd10ca18219f515e99acbf8e6bf056ca
Ditcher <Ditcher@DESKTOP-1H10LPM>
2016-05-07 18:38:11 +0300

    Third print

--- maksym_krasnoshchekov/src/main/java/action03/App.java
+++ maksym_krasnoshchekov/src/main/java/action03/App.java
@@ -8,5 +8,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello second World!");
+		System.out.println("Hello third World!");
 	}
 }
