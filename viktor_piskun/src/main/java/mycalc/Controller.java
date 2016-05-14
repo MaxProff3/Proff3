5936400d4fb5ffd4840019c64b8339d67fa16b76
squeaker01 <viktor.piskun@ukr.net>
2016-05-14 18:12:51 +0300

    fxcalc

--- viktor_piskun/src/main/java/mycalc/Controller.java
+++ viktor_piskun/src/main/java/mycalc/Controller.java
@@ -9,35 +9,12 @@ public class Controller {
 	@FXML private TextField display;
 	@FXML private Button eql;
 	@FXML private Calc calc = new Calc();
-//	public Button button;
 	
 	@FXML
 	public void doSomething(ActionEvent event) {
-		char p = ((Button)event.getSource()).getText().charAt(0);		
+		Button button = ((Button)event.getSource());
+		char p = button.getText().charAt(0);		
 		calc.inChar(p);
 		display.setText(calc.getResult());
 	}
 }
-
-//	private void processNum(ActionEvent event) {
-//		char digit = ((Button)event.getSource()).getText().charAt(0);
-//		String oldTxt = display.getText();
-//		String newTxt = oldTxt + digit;
-//		if(oldTxt.equals("0")) {
-//			newTxt = "" + digit;
-//		}
-//		calc.inChar(digit);
-//		display.setText(newTxt);
-//	}
-
-//	private void processOp(ActionEvent event) {
-//		char x = ((Button)event.getSource()).getText().charAt(0);
-//		calc.inChar(x);
-//		System.out.println(x);
-//	}
-
-//	private void processEql(ActionEvent event) {
-//		char x = ((Button)event.getSource()).getText().charAt(0);
-//		calc.inChar(x);
-//		System.out.println(x);
-//	}
