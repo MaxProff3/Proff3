59618d98c8a3368d053713119d853b56c9d79f76
Maks Coach <maks_coach@ukr.net>
2016-05-14 12:19:55 +0300

    reflectionaPI

---
+++ proff3_example/src/main/java/action05/ReflectionApi.java
@@ -0,0 +1,27 @@
+package action05;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import action04.Car;
+
+public class ReflectionApi {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		ReflectionApi obj = new ReflectionApi();
+		Class<?> cl = obj.getClass();
+		new Car().getClass();
+		Class<?> cl1 =Car.class;
+		ReflectionApi obj1 = (ReflectionApi)cl.newInstance();
+		
+		Field[] fields = cl.getFields();
+		cl.getDeclaredFields();
+		
+		Method[] methods = cl.getMethods();
+		cl.getDeclaredMethods();
+		
+		cl.getInterfaces();
+		cl.getSuperclass();
+
+ 	}
+	
+}
