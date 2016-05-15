9d6b254417d91fa32975e9d0e222c1b1cd8ac0aa
Maks Coach <maks_coach@ukr.net>
2016-05-15 13:40:55 +0300

    proff3_example

--- proff3_example/src/main/java/action05/ThreadExample.java
+++ proff3_example/src/main/java/action05/ThreadExample.java
@@ -9,6 +9,9 @@ public class ThreadExample {
 		//obj2.setDaemon(true);
 		obj2.start();
 		System.out.println("\t\tend");
+		obj2.stop();
+		obj2.interrupt();
+		
 	}
 }
 
@@ -19,6 +22,7 @@ class MyThread extends Thread {
 	public void run() {
 		synchronized (mutex) {
 			for (int i = 0; i < 2000; i++) {
+				if(isInterrupted()){break;}
 				System.out.println(getName() + "_" + i);
 			}
 		}
