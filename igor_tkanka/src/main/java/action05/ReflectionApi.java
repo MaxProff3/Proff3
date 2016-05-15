a035e79728dd2ac35fbdc03582384c889ffc727e
Kayenushka <kayen@ukr.net>
2016-05-15 13:52:04 +0300

    update

---
+++ igor_tkanka/src/main/java/action05/ReflectionApi.java
@@ -0,0 +1,28 @@
+package action05;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import action04.Car;
+
+public class ReflectionApi {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		ReflectionApi obj = new ReflectionApi();
+
+		Class<?> cl = obj.getClass();//cl хранит тип1 obj
+
+		ReflectionApi obj1 = (ReflectionApi)cl.newInstance();
+
+		
+		Field[] fields = cl.getFields();
+		cl.getDeclaredFields();
+		
+		Method[] methods = cl.getMethods();
+		cl.getDeclaredMethods();
+		
+		cl.getInterfaces();  //может быть несколько одинаковых
+		cl.getSuperclass();
+		
+		methods[0].isAnnotationPresent(MyAnnotation.class);
+ 	}	
+}
