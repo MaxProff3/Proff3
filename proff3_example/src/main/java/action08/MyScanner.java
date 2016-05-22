3f1e461fb2aad2fb2da5271f97909029442255a4
squeaker01 <viktor.piskun@ukr.net>
2016-05-22 14:17:31 +0300

    fix

---
+++ proff3_example/src/main/java/action08/MyScanner.java
@@ -0,0 +1,28 @@
+package action08;
+
+import java.util.Scanner;
+
+public class MyScanner {
+	public static void main(String[] args) throws IOException {
+		/*
+		Reader system = new InputStreamReader(System.in);
+		BufferedReader buff = new BufferedReader(system);
+		String str = buff.readLine();
+		System.out.println(str);
+		*/
+		//
+		Scanner scan = new Scanner(System.in);
+		/*
+		if(scan.hasNextInt()){
+			int k = scan.nextInt();
+			System.out.println("1");
+		}
+		if(scan.hasNextInt()){
+			int k = scan.nextInt();
+			System.out.println("2");
+		}
+		*/
+		System.out.println(scan.nextLine());
+		System.out.println(scan.nextLine());
+	}
+}
