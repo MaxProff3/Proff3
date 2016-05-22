b52cdc39f9af70ae19e9ae30f2a58a6a7b9b3a11
Maks Coach <maks_coach@ukr.net>
2016-05-22 13:30:28 +0300

    action08

--- proff3_example/src/main/java/action08/MyScanner.java
+++ proff3_example/src/main/java/action08/MyScanner.java
@@ -3,7 +3,7 @@ package action08;
 import java.util.Scanner;
 
 public class MyScanner {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		/*
 		Reader system = new InputStreamReader(System.in);
 		BufferedReader buff = new BufferedReader(system);
@@ -12,17 +12,17 @@ public class MyScanner {
 		*/
 		//
 		Scanner scan = new Scanner(System.in);
-		/*
+		
 		if(scan.hasNextInt()){
-			int k = scan.nextInt();
-			System.out.println("1");
+			int k1 = scan.nextInt();
+			System.out.println("1, k1="+k1);
 		}
 		if(scan.hasNextInt()){
-			int k = scan.nextInt();
-			System.out.println("2");
+			int k2 = scan.nextInt();
+			System.out.println("2, k2="+k2);
 		}
-		*/
+		
 		System.out.println(scan.nextLine());
-		System.out.println(scan.nextLine());
+		//System.out.println(scan.nextLine());
 	}
 }
