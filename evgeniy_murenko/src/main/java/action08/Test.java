216e2d1e1862b2a13278157f119c1f9a488ae3b9
evgeniy_murenko <mr_jeka@bk.ru>
2016-05-28 11:02:15 +0300

    MyScanner + Chat

---
+++ evgeniy_murenko/src/main/java/action08/Test.java
@@ -0,0 +1,22 @@
+package action08;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		try {
+			Socket s = new Socket("127.0.0.1", 33387);
+			System.out.println(s.toString());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
