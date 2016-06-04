0e4674ddef6a61a742aeef405d2e91fa51731322
Kayenushka <kayen@ukr.net>
2016-06-04 13:13:40 +0300

    test

---
+++ igor_tkanka/src/main/java/domain/Product.java
@@ -0,0 +1,67 @@
+package domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="product")
+public class Product {// POJO
+	@Id
+	@GeneratedValue(generator = "increment2")
+	@GenericGenerator(name = "increment2", strategy = "increment")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="code")
+	private int barcode = 0;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(int barcode) {
+		this.barcode = barcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", barcode=" + barcode + "]";
+	}
+
+	public Product() {
+	}
+
+
+	public Product(String name, int barcode) {
+		this.name = name;
+		this.barcode = barcode;
+	}
+
+	public Product(String name) {
+		this(name,0);
+	}
+	
+}
