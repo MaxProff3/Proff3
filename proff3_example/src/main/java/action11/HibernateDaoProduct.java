6fbba79bec56418ddacb350e922218fa7576f88a
Maks Coach <maks_coach@ukr.net>
2016-06-04 13:35:03 +0300

    HibernateProductDao

--- proff3_example/src/main/java/action11/HibernateDaoProduct.java
+++ proff3_example/src/main/java/action11/HibernateDaoProduct.java
@@ -2,27 +2,18 @@ package action11;
 
 import java.util.List;
 
-import org.hibernate.SessionFactory;
-
-import dao.ContructorDao;
-import dao.ContructorDaoImpl;
 import dao.ProductDao;
 import dao.ProductDaoImpl;
-import domain.Contructor;
+import domain.Product;
 import util.HibernateUtil;
 
 public class HibernateDaoProduct {
 	public static void main(String[] args) {
-		SessionFactory factory = HibernateUtil.getSessionFactory();
-		ProductDao productDao = new ProductDaoImpl(factory);
+		ProductDao productDao = new ProductDaoImpl();
 
-		// contructorDao.create(new Contructor("Зкешенко"));
-
-		List<Product> contructors = productDao.findAll();
-		System.out.println(contructors);
+		List<Product> products = productDao.findAll();
+		System.out.println(products);
 		
-		if(factory!=null){
-			factory.close();
-		}
+		HibernateUtil.getSessionFactory().close();
 	}
 }
