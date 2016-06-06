e42b697c28f4278ea1d0296161f59f43982ce2d6
Oleg <nkoleg@mail.ru>
2016-06-06 22:20:20 +0300

    action12

---
+++ oleg_koshel/src/main/java/domain/Contructor.java
@@ -0,0 +1,59 @@
+package domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="contructors")
+public class Contructor {
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+
+	
+	public Contructor(){
+			
+	} 
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Contructor " + name;
+	}
+
+}
